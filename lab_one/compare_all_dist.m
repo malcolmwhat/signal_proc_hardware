8848df2c8c79131e34db04fc05db429a303d90cf
David Lavoie-Boutin <d.lavoie.boutin@me.com>
2016-09-27 18:57:13 -0400

    Debug spectrum generation

--- lab_one/compare_all_dist.m
+++ lab_one/compare_all_dist.m
@@ -1,20 +1,22 @@
+clc; clear;
 enc = encoder_633(generate_binary_values(3) );
 table = zeros(size(enc));
+
 for i = 1:size(enc,1)
-for j = 1:size(enc,1)
-table(i,j) = distance(enc(i,:), enc(j,:));
-end
-end
-for i = 1:size(table,1)
-    for j = j+1:size(table,1)
-        output(i+j) = table(i,j);
+    for j = 1:size(enc,1)
+        table(i,j) = distance(enc(i,:), enc(j,:));
     end
 end
-% output(3:size(output,1)-1)
+
+for i = 1:size(table,1)
+    for j = i+1:size(table,1)
+        table(i,j) = 0;
+    end
+end
 
 figure
-h = histogram(output(3:15));
-h.BinLimits = [0,6];
+h = histogram(table);
+h.BinLimits = [1,6];
 
 xlabel({'Distance'},'FontSize',11);
 title({'(6, 3, 3) Code Spectrum'},'FontSize',11);
