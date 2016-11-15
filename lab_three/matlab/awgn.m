28246e1f8b3961857ce6ebe3f9ce56f7336ff191
David Lavoie-Boutin <d.lavoie.boutin@me.com>
2016-11-15 15:58:22 -0500

    Additive White Gaussian Noise to  2d image

---
+++ lab_three/matlab/awgn.m
@@ -0,0 +1,13 @@
+function [ output_args ] = awgn( image, snr )
+%AWGN Add gaussian noise to some image with some snr
+
+power_signal = mean(mean(image));
+power_noise = power_signal / 10^(snr / 20);
+
+noise = randn(size(image)) * power_noise;
+size(noise)
+
+output_args = uint8(double(image) + noise);
+
+end
+
